7949b2aa3ea37bdc89846993f5f7ebf71c538c40
George Cuculescu <umblaciosu@gmail.com>
2026-01-05 12:41:51 +0000

    updated values with exit_profit_only

--- chart/strategies/sample_strategy.py
+++ chart/strategies/sample_strategy.py
@@ -60,7 +60,7 @@ class SampleStrategy(IStrategy):
 
     # These values can be overridden in the "ask_strategy" section in the config.
     use_sell_signal = True
-    sell_profit_only = False
+    exit_profit_only = False
     ignore_roi_if_buy_signal = False
 
     # Number of candles the strategy requires before producing valid signals
